a34eec58f5c73245532647aa8d902c0658964749
netpoetica <kthrosenberg@gmail.com>
2019-01-19 12:58:18 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Start the API server
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
